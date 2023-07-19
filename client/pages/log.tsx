76502c14a1c25986db76dfca087eecee07af7aac
osawa-koki <github@osawa.dev>
2023-07-19 22:36:08 +0900

    ページネーション機能を追加した。
    ページネーション機能を追加した。

--- client/pages/log.tsx
+++ client/pages/log.tsx
@@ -1,9 +1,11 @@
-import React, { useMemo } from 'react'
-import Layout from '../components/Layout'
-import setting from '../setting'
-import { Table } from 'react-bootstrap'
+import React, { useEffect, useMemo, useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { Pagination, Table } from 'react-bootstrap'
 import useSWR from 'swr'
 import dayjs from 'dayjs'
+import Layout from '../components/Layout'
+import setting from '../setting'
 
 type Portscan = {
   id: number
@@ -21,7 +23,27 @@ type Pagination = {
   total_count: number
 }
 
+const emptyFunction = () => {}
+const fetcher = (url: string) => fetch(url).then((res) => res.ok ? res.json() : null)
+
 function TableComponent (): JSX.Element {
+
+  const [page, setPage] = useState<number>(1)
+  const router = useRouter()
+  const [firstLock, setFirstLock] = useState(false)
+  useEffect(() => {
+    const _page = router.query.page
+    const page = (typeof _page === 'string' ? _page : _page?.join('')) ?? null
+    if (page != null) setPage(parseInt(page ?? '1'))
+    setFirstLock(true)
+  }, [router.query.page])
+  useEffect(() => {
+    if (!firstLock) return
+    router.replace({
+      pathname: '/log/',
+      query: { page }
+    }).then(emptyFunction).catch(emptyFunction)
+  }, [page])
 
   const {
     data,
@@ -29,7 +51,7 @@ function TableComponent (): JSX.Element {
   } = useSWR<{
     data: Portscan[]
     pagination: Pagination
-  }>(`${setting.apiPath}/api/portscans`, (url) => fetch(url).then((res) => res.json()))
+  }>(`${setting.apiPath}/api/portscans?page=${page}`, fetcher)
 
   const {
     portscans,
@@ -45,7 +67,15 @@ function TableComponent (): JSX.Element {
   if (!data) return <div>loading...</div>
 
   return (
-    <Table striped bordered hover>
+    <>
+    <Pagination className='mt-3'>
+      <Pagination.First onClick={() => {setPage(1)}} disabled={page == 1} />
+      <Pagination.Prev onClick={() => {setPage(pagination!.current_page - 1)}} disabled={pagination?.prev_page == null} />
+      <Pagination.Item>{pagination?.current_page}</Pagination.Item>
+      <Pagination.Next onClick={() => {setPage(pagination!.current_page + 1)}} disabled={pagination?.next_page == null} />
+      <Pagination.Last onClick={() => {setPage(pagination!.total_pages)}} disabled={page == pagination?.total_pages} />
+    </Pagination>
+    <Table className='mt-3' striped bordered hover>
       <thead>
         <tr>
           <th>ID</th>
@@ -57,7 +87,9 @@ function TableComponent (): JSX.Element {
       <tbody>
         {portscans?.map((portscan) => (
           <tr key={portscan.id}>
-            <td>{portscan.id}</td>
+            <td>
+              <Link href={`/result/?id=${portscan.id}`}>#{portscan.id}</Link>
+            </td>
             <td>{portscan.host}</td>
             <td>{portscan.ip_address}</td>
             <td>{dayjs(portscan.created_at).format('YYYY-MM-DD HH:mm:ss')}</td>
@@ -65,6 +97,7 @@ function TableComponent (): JSX.Element {
         ))}
       </tbody>
     </Table>
+    </>
   )
 }
 
