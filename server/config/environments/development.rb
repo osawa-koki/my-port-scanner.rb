3f1ceeb0bc0a46adbf5bf9ffa17a25adbdca64e1
osawa-koki <github@osawa.dev>
2023-07-19 20:12:29 +0900

    CORS許可した。

--- server/config/environments/development.rb
+++ server/config/environments/development.rb
@@ -63,4 +63,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins "http://localhost:3000"
+      resource "*",
+        headers: :any,
+        methods: [:get, :post]
+    end
+  end
 end
