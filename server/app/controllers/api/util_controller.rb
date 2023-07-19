122c166c1db439aea4ea664161ebc8a950962a4c
osawa-koki <github@osawa.dev>
2023-07-19 18:42:50 +0900

    ポートスキャンAPIの作成。

---
+++ server/app/controllers/api/util_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'socket'
+require 'timeout'
+
+module Api
+  class UtilController < ApplicationController
+    def portscan
+      portscan_instance = Portscan.new(permitted_params)
+      unless portscan_instance.valid?
+        render json: { errors: portscan_instance.errors.full_messages }, status: :unprocessable_entity
+        return
+      end
+      results = portscan_instance.start_scanning
+      portscan_instance.save!
+      render json: results
+    end
+
+    private
+
+    def permitted_params
+      params.permit(:host, :port_start, :port_end, :timeout_second, :thread_count)
+    end
+  end
+end
